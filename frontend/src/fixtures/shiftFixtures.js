c85541e4eaceb197ade9085d6246d7c7a18a2c44
Ian Wen <h_wen@ucsb.edu>
2023-06-07 19:46:53 -0700

    iw - fixed shift time format for consistency with backend

--- frontend/src/fixtures/shiftFixtures.js
+++ frontend/src/fixtures/shiftFixtures.js
@@ -3,24 +3,24 @@ const shiftFixtures = {
         {
             "id": 1,
             "day": "Monday",
-            "shiftStart": "8AM",
-            "shiftEnd": "11AM",
+            "shiftStart": "08:00AM",
+            "shiftEnd": "11:00AM",
             "driverID": 1,
             "driverBackupID": 3
         },
         {
             "id": 2,
             "day": "Tuesday",
-            "shiftStart": "11AM",
-            "shiftEnd": "2PM",
+            "shiftStart": "11:00AM",
+            "shiftEnd": "02:00PM",
             "driverID": 2,
             "driverBackupID": 1
         },
         {
             "id": 3,
             "day": "Thursday",
-            "shiftStart": "3PM",
-            "shiftEnd": "6PM",
+            "shiftStart": "03:00PM",
+            "shiftEnd": "06:00PM",
             "driverID": 3,
             "driverBackupID": 2
         },
